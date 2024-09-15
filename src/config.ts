d40173c94d93fd6b45dca1c931e4cafdc0870de3
David Dobondi <david.dobondi@neho.ch>
2024-09-15 17:27:00 +0300

    initial commit

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+export const SITE_TITLE = 'David Dobondi | Web Developer | Tech Enthusiast';
+export const SITE_DESCRIPTION = 'Experienced web developer passionate about creating innovative and user-friendly digital solutions.';
+export const GENERATE_SLUG_FROM_TITLE = true
+export const TRANSITION_API = true
